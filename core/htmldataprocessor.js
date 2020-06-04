53494c9933e3573732db7770e2c920de822ce942
Hidekazu Kubota <hidekazu.kubota@gmail.com>
2020-06-04 21:05:26 +0900

    change: Enable <script> tag

--- core/htmldataprocessor.js
+++ core/htmldataprocessor.js
@@ -899,7 +899,9 @@
 		var regexes = [
 			// Script tags will also be forced to be protected, otherwise
 			// IE will execute them.
-			( /<script[\s\S]*?(<\/script>|$)/gi ),
+      
+      // Commented it out to enable <script> tag
+      // ( /<script[\s\S]*?(<\/script>|$)/gi ),
 
 			// <noscript> tags (get lost in IE and messed up in FF).
 			/<noscript[\s\S]*?<\/noscript>/gi,
